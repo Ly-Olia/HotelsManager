5e74c17217d4fd985413af695e64a2dff33759eb
Olha Zahoruiko <lyolia@olhas-laptop.home>
2025-01-07 22:39:08 +0100

    Add utils file for data import

---
+++ hotels/utils.py
@@ -0,0 +1,9 @@
+# hotels/utils.py
+
+# Constants for cities and hotels CSV import
+CITY_CSV_URL = "http://rebecca.maykinmedia.nl/djangocase/city.csv"
+
+HOTEL_CSV_URL = "http://rebecca.maykinmedia.nl/djangocase/hotel.csv"
+
+USERNAME = "python-demo"
+PASSWORD = "*"
