d84c37c34d8e3ed6f6c73635103b7f6888dcfe31
Olha Zahoruiko <lyolia@olhas-laptop.home>
2025-01-07 23:58:23 +0100

    Crontab to import data every day

--- hotels/scripts/import_data.sh
+++ hotels/scripts/import_data.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-# Activate the virtual environment
-# This command activates the virtual environment where your Django project dependencies are installed.
-source /Users/lyolia/PycharmProjects/IntegratingThirdParties/.venv/bin/activate
+# Make sure to change the path to your actual .venv directory
+source /Users/lyolia/PycharmProjects/HotelsManager/.venv/bin/activate
 
-# Navigate to the project directory
-# This command changes the current working directory to your Django project folder.
-cd /Users/lyolia/PycharmProjects/IntegratingThirdParties
+# Ensure the path is correct for your project location
+cd /Users/lyolia/PycharmProjects/HotelsManager/
 
 # Run the import commands
-# These commands will run the Django management commands to import cities and hotels data.
-# Make sure the corresponding custom management commands (import_cities, import_hotels) are implemented in your Django project.
 python manage.py import_cities
 python manage.py import_hotels
-
-# Deactivate the virtual environment
-# This command deactivates the virtual environment after the task is completed.
-deactivate
